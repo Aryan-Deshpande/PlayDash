a850d221bc6e3d0b6b9b8ac14584d6cc7e749471
Aryan Deshpande <deshpandearyan1@gmail.com>
2022-08-19 15:18:21 +0530

    Fixed API Key Generation

--- Api/api_gen.py
+++ Api/api_gen.py
@@ -6,13 +6,25 @@ from backend.db import cur, connection
 # generate api key, and token to get access to the API
 # api key for vendors
 
+## The Vendor Requires A password & username created at agreement
+
 @app.route('/api/v1/gen',methods=['POST','GET'])
 def genaapikey():
-        apiKey = secrets.token_urlsafe(32)
-        cur.execute('UPDATE vendor SET token=%s WHERE %s',(apiKey,))
-        connection.commit()
-    #else:
-        return jsonify(apiKey)
+        if (request.json['username'] is not None) and (request.json['password'] is not None):
+            uname = request.json['username']
+            password = request.json['password']
+
+            cur.execute('SELECT * FROM vendor WHERE uname=%s AND password=%s',(uname,password))
+            if cur.fetchone() is not None:
+                apiKey = secrets.token_urlsafe(32)
+                cur.execute('UPDATE vendor SET token=%s WHERE uname=%s',(apiKey, uname))
+                connection.commit()
+                return jsonify(apiKey)
+            
+            return jsonify('Access Denied 1')
+            
+
+        return jsonify('Access Denied 2')
 
 # api for the particular vendor
 @app.route('/api/v1/getdata', methods=['GET'])
@@ -21,14 +33,15 @@ def getdata():
         data = {}
         token = request.headers['HTTP_X_API_KEY']
 
-        cur.execute('SELECT id FROM vendor WHERE token=%s',(token,))
+        cur.execute('SELECT * FROM vendor WHERE token=%s',(token,))
         obj = cur.fetchone()
-
+        print(type(obj))
         if obj is None:
             return jsonify('create a token first')
+        
 
         else:
-            vid = obj[0]
+            vid = obj[0][0]
             cur.execute('SELECT * FROM events WHERE vid=%s', (vid,))
             obj = [cur.fetchone()]
 
