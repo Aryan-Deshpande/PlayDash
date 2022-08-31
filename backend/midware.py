b5e429fbc277a749efe16da6a1d0656a86bc3334
Aryan Deshpande <deshpandearyan1@gmail.com>
2022-08-31 10:03:42 +0530

    Fixed backend / Need to test with frontend

--- backend/midware.py
+++ backend/midware.py
@@ -1,40 +1,80 @@
 import psycopg2
 import os
-from backend.db import cur, connection
+from backend.db import cur, connection #add backend.db
 
 def checkuser(username,password):
-    username,hashed_password = cur.execute("SELECT * FROM users WHERE username = %s AND password = %s", (username,password))
-    unhashed_password = 1 # unhash
     if username and password:
-        if password ==  unhashed_password:
-            return True
+        cur.execute("SELECT id FROM uses WHERE name=%s AND password=%s", (username,password))
+        if cur.fetchone() is not None:
+            print('in here')
+            cur.execute("SELECT name FROM uses WHERE name=%s AND password=%s", (username,password))
+            # unhash the password
+            unhpassword = password
+            if password == unhpassword:
+                return True
+            else:
+                return False
         else:
             return False
-    else:
-        return False
+    return False
 
 def register(username,password):
-    try:
-        obj = cur.execute("INSERT INTO users.person (username,password) values(%s,%s)", (username,password))
-        return True
-    except:
-        print("not able to insert person")
+    obj1 = cur.execute('SELECT name FROM uses WHERE name=%s',(username,))
+    if obj1 is None:
+        try:
+            obj = cur.execute("INSERT INTO uses (name,password) values (%s,%s)", (username,password))
+            print(obj)
+            connection.commit()
+            return True
+        except:
+            return False
+    return False
 
 def createBooking(userId,eventId,eventName): # include later, timeSlot
-    obj = cur.execute("UPDATE events SET usesid=%s WHERE id=%s AND name=%s", (userId,eventId,eventName))
-    connection.commit()
+    # check if user has event already booked
+    cur.execute("SELECT id FROM uses WHERE name=%s",(userId,))
+    query1 = cur.fetchone()
+    print(cur.fetchone())
+    userId = query1.item()
+    obj1 = cur.execute('SELECT id FROM events WHERE usesid=%s',(userId,))
+    print(obj1.fetchone())
+    if obj1.fetchone()[0] is not None:
+        return False
+
+    try:
+        print(userId)
+        obj = cur.execute("UPDATE events SET usesid=%s WHERE id=%s AND name=%s", (userId,eventId,eventName))
+        connection.commit()
+        return True
+    except:
+        return False
+
 
 # get page details
-def pageApi(eventName):
+def pageFunc(eventName):
     cur.execute("SELECT * FROM events WHERE name=%s",(eventName,))
     return cur.fetchone()  # You can also use curr.fetchall() if you have multiple output rows 
     
-def pageApis():
+def pageFuncs():
     cur.execute('SELECT id,name FROM events')
     obj = cur.fetchall()
     return obj
 
-cur.execute('SELECT id FROM vendor WHERE token=%s',('hvz1QMGwW1rrRln-FYeruHOSwYEdXpEDB1mzOecSXrw',))
+def checkbooking(userId, eventId):
+    try:
+        cur.execute('SELECT usesid FROM events WHERE id=%s',(eventId,))
+        if cur.fetchone()[0] is not None:
+            if cur.fetchone()[0] == userId:
+                return True
+            else:
+                return {"recipient":False} 
+        else:
+            return False
+    except:
+        return False
+
+
+"""cur.execute('SELECT id FROM vendor WHERE token=%s',('hvz1QMGwW1rrRln-FYeruHOSwYEdXpEDB1mzOecSXrw',))
 a = cur.fetchall()
-print(a[0][0])
+print(a[0][0])"""
 
--- backend/routes.py
+++ backend/routes.py
@@ -1,64 +1,90 @@
 from backend import app
 from flask import request, jsonify, make_response, render_template, session, redirect 
-from backend.midware import checkuser, register, createBooking, pageApi, pageApis
+from backend.midware import checkuser, createBooking, pageFunc, pageFuncs, checkbooking
+from backend.midware import register as registering
 from backend.db import cur
 
+
+# Tested, it should register following user, and create session for that user
 @app.route('/register',methods=['GET','POST'])
 def register():
     if request.method == 'POST':
-        if register(request.json['username'],request.json['password']):
-            session[request.json['username']]
-        else:
-            make_response({'missing username or password'})
-    else:
-        return render_template('register.html')
 
+        if registering(request.json['Username'],request.json['Password']):
+            session[request.json['Username']] = request.json['Username'] 
+            return redirect('/events')
+        else:
+            return 'wrong'
+    else:
+        return 'html page for registering,/ not needed because of React'
+
+# Tested, it should login a user and redirect to a url
 @app.route('/login',methods=['GET','POST'])
 def login():
     if request.method == 'POST':
-        if checkuser(request.json['username'],request.json['password']):
-            session[request.json['username']]
-            redirect('/events')
-        else:
-            make_response({'Wrong password or user does not exist'})
-    else:    
-        return render_template('login.html')
 
+        if not session.get('name'):
+            return redirect('/events')
+        username = request.json['Username']
+
+        if checkuser(username,request.json['Password']):
+            session[username] = username
+            return redirect('/events')
+        else:
+            return redirect('login')
+    return render_template('login.html') # not needed due to React
+
+# Tested, should logout user / set user value in session to be None, then redirect user
 @app.route('/logout',methods=['POST'])
 def logout():
     session[session.get("name")] = None
     redirect('/login')
 
-# create a booking for a particular event
-@app.route('/event/Booking', methods=['POST'])
+# Tested, create a booking for a particular event
+@app.route('/event/Booking',methods=['POST'])
 def Booking():
-    #payemnt 
-    userId = request.json['userId']
-#    timeSlot = request.json['timeSlot'] later implementation
+    userId = session.get('name')
+    #    timeSlot = request.json['timeSlot'] later implementation
     eventId = request.json['eventId']
     eventName = request.json['eventName']
-    createBooking(userId,eventId,eventName)
+
+    """if session.get("name") is None and request.json['Booked'] == True:
+        return jsonify({"Already booked"})"""
+    try:
+        createBooking(userId,eventId,eventName)
+    except:
+        return jsonify({"Error in booking"})
+
     return jsonify('success')
 
-# list all the events available
+
+# Tested, list all the events available
 @app.route('/events',methods=['GET'])
 def events():
     eventObject = {} # JSON object to be appended and returned to the URI call
 
-    eventPages = pageApis() # Queries the postgres database with all the necessary details
+    eventPages = pageFuncs() # Queries the postgres database with all the necessary details
     for i,event in enumerate(eventPages): # Creates a new dict key with the following event details
         (eventId,eventName) = event
         eventObject[i] = [eventId,eventName]    
     return jsonify(eventObject)    
 
-# specific event page info
+# Tested, specific event page info
 @app.route('/event/{eventName}',methods=['GET'])
 def eventPage(eventName):
-    (eventId, eventName, usesId) = pageApi
-    if usesId is not None:
-        usesId = True
-    return jsonify(eventId,eventName,usesId)
+    userId = session.get("name")
 
+    (eventId, eventName, usesId) = pageFunc() # if uses id exists then remove booking functionality from webpage
+
+    res = checkbooking(userId,eventId) 
+    if res is True:
+        return jsonify({eventId,eventName,usesId},{"booked":True,"recipient":True})
+    elif res["recipient"] == False:
+        return jsonify({eventId,eventName,usesId},{"booked":True,"recipient":res["recipient"]})
+    else:   
+        return jsonify({eventId,eventName,usesId},{"booked":False,"recipient":False})
+
+""" Only for testing """
 @app.route('/test',methods=['GET'])
 def test():
     return jsonify('success', 403)
