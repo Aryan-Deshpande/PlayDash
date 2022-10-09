395d2f88f4f12fc4718db04fd778e2028422c113
Aryan Deshpande <deshpandearyan1@gmail.com>
2022-10-10 00:59:53 +0530

    Fixed Cloud Database Connection

--- backend/db.py
+++ backend/db.py
@@ -1,13 +1,23 @@
 import os
 import psycopg2
 
-connection = psycopg2.connect(
-    host='localhost', 
-    database='flasksql',
-    user='postgres',
-    password='aryan2002',
-)
-cur = connection.cursor()
+def connect():
+    connection = psycopg2.connect(
+    host=os.environ.get('DB_HOST'),
+    database=os.environ.get('DB_NAME'),
+    user=os.environ.get('DB_USER'),
+    password=os.environ.get('DB_PASS'),
+    port=os.environ.get('DB_PORT')
+    )
+    cur = connection.cursor()
+
+    cur.execute('CREATE TABLE IF NOT EXISTS vendor (id SERIAL PRIMARY KEY, uname VARCHAR(50), password VARCHAR(50), token VARCHAR(50))')
+    cur.execute('CREATE TABLE IF NOT EXISTS events (id SERIAL PRIMARY KEY, vid INTEGER, uid INTEGER, event VARCHAR(50))')
+    cur.execute('CREATE TABLE IF NOT EXISTS uses (id SERIAL PRIMARY KEY, name VARCHAR(50))')
+    connection.commit()
+
+    return cur, connection
+
 
 # used by server.py, mw.py
 # use cursor to perform database operations
